b186e72564272ad6cad673a03e174a650fd45590
yungshun317 <seraph317@gmail.com>
2021-06-08 17:05:01 +0800

    [FEAT] Use the `Navbar` component for branding and navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 export default function App() {
 	return (
 		<Container fluid>
+		    <Navbar bg="light" expand="lg">
+		        <Navbar.Brand href="#home">Business Website</Navbar.Brand>
+		        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+		        <Navbar.Collapse id="basic-navbar-nav">
+		        </Navbar.Collapse>
+		    </Navbar>
 		    <Row>
 		        <Col xs={12} sm={6} lg={8} className="bg-info text-white">xs=12 sm=6 lg=8</Col>
 		        <Col sm={6} lg={4} className="bg-primary text-white">sm=6 lg=4</Col>
