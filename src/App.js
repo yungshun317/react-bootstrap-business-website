dc0e169fade77a9cb657c7f62db0e2b9858fd9ed
yungshun317 <seraph317@gmail.com>
2021-06-08 17:12:48 +0800

    [FEAT] Use the `Nav` and `NavDropdown` components in `Navbar`

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
@@ -11,6 +13,17 @@ export default function App() {
 		        <Navbar.Brand href="#home">Business Website</Navbar.Brand>
 		        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 		        <Navbar.Collapse id="basic-navbar-nav">
+		            <Nav className="mr-auto">
+		                <Nav.Link href="#home">Home</Nav.Link>
+		                <Nav.Link href="#link">Link</Nav.Link>
+		                <NavDropdown title="Dropdown" id="basic-navbar-dropdown">
+		                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+		                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+		                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+		                    <NavDropdown.Divider />
+		                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+		                </NavDropdown>
+		            </Nav>
 		        </Navbar.Collapse>
 		    </Navbar>
 		    <Row>
