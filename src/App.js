bfb6dbf58949eb95c89533a82c8a724aaefe5b1f
yungshun317 <seraph317@gmail.com>
2021-06-08 17:48:28 +0800

    [FEAT] Create the `Layout` component to take responsibility for Bootstrap's grid system

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+
+import Layout from "./Layout";
 
 export default function App() {
 	return (
-		<Container fluid>
+		<>
 		    <Navbar bg="light" expand="lg">
 		        <Navbar.Brand href="#home">Business Website</Navbar.Brand>
 		        <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -26,19 +25,10 @@ export default function App() {
 		            </Nav>
 		        </Navbar.Collapse>
 		    </Navbar>
-		    <Row>
-		        <Col xs={12} sm={6} lg={8} className="bg-info text-white">xs=12 sm=6 lg=8</Col>
-		        <Col sm={6} lg={4} className="bg-primary text-white">sm=6 lg=4</Col>
-		    </Row>
-		    <Row>
-		        <Col xs={6} sm={4} className="bg-info text-white">xs=6 sm=4</Col>
-		        <Col xs={6} sm={4} className="bg-primary text-white">xs=6 sm=4</Col>
-		        <Col xs={6} sm={4} className="bg-success text-white">xs=6 sm=4</Col>
-		    </Row> 
-		    <Row>
-		        <Col xs={6} className="bg-info text-white">xs=6</Col>
-		        <Col xs={6} className="bg-primary text-white">xs=6</Col>
-		    </Row>
-		</Container>
+		    <Layout />
+		    <footer>
+		        This is footer.
+		    </footer>
+		</>
 	);
 }
---
+++ src/Layout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Layout() {
+	return (
+		<Container fluid>
+		    <Row>
+		        <Col xs={12} sm={6} lg={8} className="bg-info text-white">xs=12 sm=6 lg=8</Col>
+		        <Col sm={6} lg={4} className="bg-primary text-white">sm=6 lg=4</Col>
+		    </Row>
+		    <Row>
+		        <Col xs={6} sm={4} className="bg-info text-white">xs=6 sm=4</Col>
+		        <Col xs={6} sm={4} className="bg-primary text-white">xs=6 sm=4</Col>
+		        <Col xs={6} sm={4} className="bg-success text-white">xs=6 sm=4</Col>
+		    </Row>
+		    <Row>
+		        <Col xs={6} className="bg-info text-white">xs=6</Col>
+		        <Col xs={6} className="bg-primary text-white">xs=6</Col>
+		    </Row>
+		</Container>
+    )
+}
